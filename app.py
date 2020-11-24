46448f6bb30a10d4ca72dc563eeb3437d96fdb4a
WachaDev <josemrr27@gmail.com>
2020-11-24 10:14:46 -0400

    Linter working properly, refactorized and podcast displayed in the console for the momment

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from jinja2 import TemplateNotFound
+import random
 
 from flask import Flask, render_template, request, redirect, flash, abort
 from flask_wtf import FlaskForm
@@ -7,6 +7,7 @@ from wtforms import StringField, PasswordField, BooleanField, FileField, TextAre
 from wtforms.validators import InputRequired, Length, EqualTo, Email, Regexp, Optional
 from bcrypt import hashpw, gensalt, checkpw
 from werkzeug.utils import secure_filename
+from jinja2 import TemplateNotFound
 
 from database import SessionLocal, init_db
 from models import Users, Podcast
@@ -21,63 +22,101 @@ DATABASE_EXCEPTION = 'Oh no! We\'re having problems from the server-side'
 IMAGE_ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 AUDIO_ALLOWED_EXTENSIONS = {'mp3', 'wav', 'ogg', 'oga', 'flac'}
 
-db = SessionLocal()
+session = SessionLocal()
 init_db()
 
-class CreateAccount(FlaskForm):
-    email = StringField('email', validators = [
-    InputRequired('This field is required.'), 
-    Email('Invalid e-mail.'), 
-    Regexp(r'[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$', 0, 'Write your e-mail address with a format someone@example.com.')
+class CreateAccountForm(FlaskForm):
+    email = StringField('email', validators=[
+        InputRequired('This field is required.'), 
+        Email('Invalid e-mail.'), 
+        Regexp(r'[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$', 
+        0,
+        'Write your e-mail address with a format someone@example.com.')
     ])
-
-    username = StringField('usrname',validators = [
-    InputRequired('This field is required.'), 
-    Length(-1, 20, '20 is the max characters long.')
+    username = StringField('usrname', validators=[
+        InputRequired('This field is required.'), 
+        Length(-1, 20, '20 is the max characters long.')
     ])
-
-    password = PasswordField('psw',validators = [
-    InputRequired('This field is required.'), 
-    EqualTo('confirm_password', 'Password must match.'), 
-    Regexp(r'(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}', 0, 'Must contain at least one  number and one \
-        uppercase and lowercase letter, and at least be between 8 and 16 characters.')
+    password = PasswordField('psw', validators=[
+        InputRequired('This field is required.'), 
+        EqualTo('confirm_password', 'Password must match.'), 
+        Regexp(r'(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}', 
+        0, 
+        'Must contain at least one  number and one uppercase and \
+            lowercase letter, and at least be between 8 and 16 characters.')
     ])
-
-    confirm_password = PasswordField('confirm_psw',validators = [InputRequired('This field is required.')])
+    confirm_password = PasswordField('confirm_psw',validators=[
+        InputRequired('This field is required.')
+    ])
 
 class LoginForm(FlaskForm):
-    username = StringField('usrname', validators = [InputRequired('A username is required.')])
-    password = PasswordField('psw', validators = [InputRequired('A password is required.')])
+    username = StringField('usrname', validators=[
+        InputRequired('A username is required.')
+    ])
+    password = PasswordField('psw', validators=[
+        InputRequired('A password is required.')
+    ])
     remember = BooleanField('remember_me')
 
-class UploadPodcast(FlaskForm):
-    title = StringField('title', validators = [InputRequired('We need a title!')])
-    image = FileField('image', validators = [Optional()])
-    audio = FileField('audio', validators = [InputRequired('We need your podcast here!')])
-    description = TextAreaField('description', validators = [Optional()])
+class UploadPodcastForm(FlaskForm):
+    title = StringField('title', validators=[
+        InputRequired('We need a title!')
+    ])
+    image = FileField('image', validators=[Optional()])
+    audio = FileField('audio', validators=[
+        InputRequired('We need your podcast here!')
+    ])
+    description = TextAreaField('description', validators=[Optional()])
 
 
 @app.route('/')
 def home():
-    try:
+    MAX_PODCASTS_TO_DISPLAY = 3
+    a = []
+    for _ in range(0, MAX_PODCASTS_TO_DISPLAY):
+        available_pdst = session.query(Podcast).count()
+        r_id = random.randint(1, available_pdst)
+        
+        encountered_pdst = session.query(Podcast.id).filter_by(id=r_id).scalar()
+        if encountered_pdst in a:
+            MAX_PODCASTS_TO_DISPLAY += 1
+            print(a)
+        else:
+            pdst_data = session.query(Podcast).filter_by(id=encountered_pdst).first()
+
+            title = pdst_data.title
+            img = pdst_data.image
+            audio = pdst_data.audio
+            description = pdst_data.description 
+            print(
+                title + "\n",
+                img + "\n",
+                audio + "\n",
+                description + "\n"
+            )
+            a.append(encountered_pdst)
+            print(a)
+
+
+    try:    
         return render_template('index.html')
     except TemplateNotFound:
         abort(404)
 
 
-@app.route('/create-account', methods = ['GET', 'POST'])
+@app.route('/create-account', methods=['GET', 'POST'])
 def create_account():
     err_email = None
     err_usr = None
-    form = CreateAccount()
+    form = CreateAccountForm()
 
     if form.validate_on_submit():
         email = form.email.data
         usrname = form.username.data
         psw = form.password.data
 
-        usrname_exists = bool(db.query(Users.id).filter_by(username = usrname).scalar())
-        email_exists = bool(db.query(Users.id).filter_by(e_mail = email).scalar())
+        usrname_exists = bool(session.query(Users.id).filter_by(username=usrname).scalar())
+        email_exists = bool(session.query(Users.id).filter_by(e_mail=email).scalar())
 
         if email_exists:
             err_email = 'This e-mail already exists.'
@@ -87,38 +126,36 @@ def create_account():
             encode_psw = hashpw(psw.encode('UTF-8'), gensalt())
 
             new_user = Users(
-                e_mail = email,
-                username = usrname,
-                password = encode_psw,
+                e_mail=email,
+                username=usrname,
+                password=encode_psw,
             )
 
             try:
-                db.add(new_user)
-                db.commit()
+                session.add(new_user)
+                session.commit()
                 return redirect('/login')
             except:
-                db.rollback()
+                session.rollback()
                 return DATABASE_EXCEPTION
-            finally:
-                db.commit()
 
     try: 
-        return render_template('create_account.html', form = form, err_email = err_email, err_usr = err_usr)
+        return render_template('create_account.html', form=form, err_email=err_email, err_usr=err_usr)
     except TemplateNotFound:
         abort(404)
             
         
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
     err_db = None
+    form = LoginForm()
     
     if form.validate_on_submit():
         usrname = form.username.data
         psw = form.password.data
         remember = form.remember.data
         
-        user_exists = db.query(Users).filter_by(username = usrname).first()
+        user_exists = session.query(Users).filter_by(username=usrname).first()
         
         if user_exists:
             check_psw = checkpw(psw.encode('UTF-8'), user_exists.password)
@@ -131,21 +168,22 @@ def login():
             err_db = 'Invalid username'
 
     try:
-        return render_template('login.html', form = form, err_db = err_db)
+        return render_template('login.html', form=form, err_db=err_db)
     except TemplateNotFound:
         abort(404)
 
 
 def allowed_file(filename, allowed_extensions):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
-@app.route('/upload', methods = ['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    form = UploadPodcast()
     err_image = None
     err_audio = None
     img_folder = app.config['UPLOAD_IMAGE_FOLDER']
     audio_folder = app.config['UPLOAD_AUDIO_FOLDER']
+    form = UploadPodcastForm()
     
     if form.validate_on_submit():
         title = form.title.data
@@ -154,9 +192,11 @@ def upload():
         description = form.description.data
 
         if not allowed_file(image.filename, IMAGE_ALLOWED_EXTENSIONS):
-            err_image = 'Invalid image format, use ' + ', '.join(IMAGE_ALLOWED_EXTENSIONS) + ' instead.'
+            err_image = 'Invalid image format, use ' + \
+                ', '.join(IMAGE_ALLOWED_EXTENSIONS) + ' instead.'
         elif not allowed_file(audio.filename, AUDIO_ALLOWED_EXTENSIONS):
-            err_audio = 'Invalid audio format, use ' + ', '.join(AUDIO_ALLOWED_EXTENSIONS) + ' instead.'
+            err_audio = 'Invalid audio format, use ' + \
+                ', '.join(AUDIO_ALLOWED_EXTENSIONS) + ' instead.'
         else:
             image_name = secure_filename(image.filename)
             img_path = os.path.join(img_folder, image_name)
@@ -167,25 +207,23 @@ def upload():
             audio.save(audio_path)
 
             new_podcast = Podcast(
-                title = title,
-                image = img_path,
-                audio = audio_path,
-                description = description,
+                title=title,
+                image=img_path,
+                audio=audio_path,
+                description=description,
             )
 
             try:
-                db.add(new_podcast)
-                db.commit()
+                session.add(new_podcast)
+                session.commit()
                 flash('Uploaded')
                 return redirect('/')
             except:
-                db.rollback()
+                session.rollback()
                 return DATABASE_EXCEPTION
-            finally:
-                db.commit()
 
     try:
-        return render_template('upload.html', form = form, err_image = err_image, err_audio = err_audio)
+        return render_template('upload.html', form=form, err_image=err_image, err_audio=err_audio)
     except:
         abort(404)
 
@@ -200,7 +238,7 @@ def play_podcast():
 
 @app.errorhandler(404)
 def not_found(e):
-    return 'Page not found', 404
+    return '<h1>Page not found</h1>', 404
 
 
 if __name__ == '__main__':
